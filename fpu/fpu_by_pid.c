a3496c8c295ca87bc646cb7a48d7c3f8460db0ab
Wa4owski <312431@niuitmo.ru>
2022-12-05 20:20:21 +0100

    got rid of global vars

--- fpu/fpu_by_pid.c
+++ fpu/fpu_by_pid.c
@@ -11,54 +11,51 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("OS. Lab2. debugfs: fpu_state");
+MODULE_AUTHOR("Pavel Vakhovskiy");
+
 
 static struct dentry *dir;
-static struct task_struct *ts1;
-static union fpregs_state fp_state;
 static struct dentry *pid_file;
 static struct dentry *fpu_file;
 
+static const int ST_SIZE = 20;
 static u32 pid = -1;
 
 static int pid_write_op(void *data, u64 value){
-	pid = value;
-	pr_info("fpu: input pid is %d.\n", pid); 
-	ts1 = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
-	return 0;
+    pid = value;
+    pr_info("fpu: input pid is %d.\n", pid);
+    return 0;
 }
 
 DEFINE_SIMPLE_ATTRIBUTE(pid_ops, NULL, pid_write_op, "%llu\n");
 
 
-
-static void print_fp_state(struct seq_file *m){
-	if(!ts1){
-		seq_printf(m, "task_struct with pid %d has not been fond.\n", pid); 
+static void print_fpu_state(struct seq_file *m, struct task_struct *ts){
+        union fpregs_state fp_state = ts->thread.fpu.state;
+        seq_printf(m, "fsave registers:\n");
+	seq_printf(m, " cwd %d // FPU Control Word\n", fp_state.fsave.cwd);
+        seq_printf(m, " swd %d // FPU Status Word\n", fp_state.fsave.swd);
+        seq_printf(m, " twd %d // FPU Tag Word\n", fp_state.fsave.twd);
+        seq_printf(m, " fip %d // FPU IP Offset\n", fp_state.fsave.fip);
+        seq_printf(m, " fcs %d // FPU IP Selector\n", fp_state.fsave.fcs);
+        seq_printf(m, " foo %d // FPU Operand Pointer Offset\n", fp_state.fsave.foo);
+	seq_printf(m, " fos %d // FPU Operand Pointer Selector\n", fp_state.fsave.fos);
+        seq_printf(m, "st_space:\n");
+        size_t count = 0;
+        while(count++ < ST_SIZE){
+            seq_printf(m, "%d\n", fp_state.fsave.st_space[count]);
         }
-	else{
-       	    	fp_state  = ts1->thread.fpu.state;
-		
-		seq_printf(m, "fsave registers:\n");
-        	seq_printf(m, " cwd %d // FPU Control Word\n", fp_state.fsave.cwd);
-	        seq_printf(m, " swd %d // FPU Status Word\n", fp_state.fsave.swd);
-		seq_printf(m, " twd %d // FPU Tag Word\n", fp_state.fsave.twd);
-		seq_printf(m, " fip %d // FPU IP Offset\n", fp_state.fsave.fip);
-		seq_printf(m, " fcs %d // FPU IP Selector\n", fp_state.fsave.fcs);
-        	seq_printf(m, " foo %d // FPU Operand Pointer Offset\n", fp_state.fsave.foo);
-	    	seq_printf(m, " fos %d // FPU Operand Pointer Selector\n", fp_state.fsave.fos);
-	    	seq_printf(m, "st_space:\n");
-	    	
-		int i = 0;
-		while(i < 20){
-		    seq_printf(m, "%d\n", fp_state.fsave.st_space[i]);
-		    i++;
-		}
-
-	}
 }
 static int show(struct seq_file *m, void *v)
 {
-    print_fp_state(m);
+    static struct task_struct *ts;
+    ts = get_pid_task(find_get_pid(pid), PIDTYPE_PID);
+    if(!ts){
+        pr_info("fpu_mod: task_struct with pid %d has not been founded.\n", pid);
+	seq_printf(m, "task_struct with pid %d has not been founded.\n", pid);
+        return 0;
+    }
+    print_fpu_state(m, ts);
     return 0;
 }
 
@@ -68,30 +65,29 @@ static int open(struct inode *inode, struct  file *file)
 }
 
 static const struct file_operations fpu_ops = {
-    .llseek = seq_lseek,
-    .open = open,
-    .owner = THIS_MODULE,
-    .read = seq_read,
-    .release = single_release,
+        .llseek = seq_lseek,
+        .open = open,
+        .owner = THIS_MODULE,
+        .read = seq_read,
+        .release = single_release,
 };
-	
-	
-	
 
-static int __init kmod_init(void) {
-    pr_info("fpu: module loading.\n");
-	
+
+static int kmod_init(void) {
+    pr_info("fpu_mod: module loading.\n");
+
     dir = debugfs_create_dir("fpu", NULL);
-    pid_file = debugfs_create_file("pid_here", 0722, dir, NULL, &pid_ops);
+    pid_file = debugfs_create_file("pid_here", 0222, dir, NULL, &pid_ops);
     fpu_file = debugfs_create_file("fpu_output", 0622, dir, NULL, &fpu_ops);
-	
+
     return 0;
 }
 
-static void __exit kmod_exit(void) {
+static void kmod_exit(void) {
     debugfs_remove_recursive(dir);
     pr_info("fpu: module unloaded\n");
 }
 
 module_init(kmod_init);
 module_exit(kmod_exit);
+
--- inode/inode_by_path.c
+++ inode/inode_by_path.c
@@ -12,6 +12,7 @@
 #include <linux/fs.h>
 #include <linux/printk.h>
 
+
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("OS. Lab2. debugfs: indode");
 MODULE_AUTHOR("Pavel Vakhovskiy");
@@ -23,71 +24,36 @@ static struct dentry *subdir;
 static struct dentry *path_file;
 static struct dentry *inode_file;
 
-static struct inode *inode;
-static struct path path;
-static char *path_name;
-
-static void print_inode(void);
+//static struct inode* get_inode(char *);
 static ssize_t data_read(struct file *, char *, size_t, loff_t *);
 static ssize_t data_write(struct file *, const char *, size_t, loff_t *);
 
-const struct file_operations path_file_ops = {
-	.owner = THIS_MODULE,
-	.write = data_write,
-	.read = data_read,
-};
-
-static void print_inode(){
-	kern_path(path_name, LOOKUP_FOLLOW, &path);
-	inode = path.dentry->d_inode;
-	pr_info("Path name : %s, inode :%lu\n", path_name, inode->i_ino);
+static struct inode* get_inode(char * path_name){
+    static struct path path;
+    kern_path(path_name, LOOKUP_FOLLOW, &path);
+    return path.dentry->d_inode;
 }
 
-static ssize_t data_read(struct file *f, char *buffer,
-		size_t len, loff_t *offset)
-{
-	return simple_read_from_buffer(buffer, len, offset, data, data_len);
-}
-
-static ssize_t data_write(struct file *f, const char *buffer,
-		size_t len, loff_t *offset)
-{
-	ssize_t ret;
-
-	ret = simple_write_to_buffer(data, PAGE_SIZE, offset, buffer, len);
-	
-
-	if (ret < 0){
-		pr_info("inode: file_path writing broke.\n"); 
-		return ret;
-	}
-	
-	path_name = data;
-	pr_info("inode: file_path is %s.\n", path_name); 
-	print_inode();
-	
-	data_len = ret;
-
-	return len;
-}
-
-static void print_fp_state(struct seq_file *m){
-	if(!inode){
-		seq_printf(m, "inode structure for file %s has not been founded.\n", path_name); 
-    	}
-	else{
-	    seq_printf(m, "Inode number: %lu\n", inode->i_ino);
-            seq_printf(m, "Permissions: %hu\n", inode->i_mode);
-            seq_printf(m, "opflags: %hu\n", inode->i_opflags);
-	    seq_printf(m, "ctime(seconds): %ld\n", inode->i_ctime.tv_sec);
-	    seq_printf(m, "mtime(seconds): %ld\n", inode->i_mtime.tv_sec);
-	    seq_printf(m, "atime(seconds): %ld\n", inode->i_ctime.tv_sec);
-	 }
+static void print_inode(struct seq_file *m, struct inode * inode){
+    seq_printf(m, "Inode number: %lu\n", inode->i_ino);
+    seq_printf(m, "umode: %hu\n", inode->i_mode);
+    seq_printf(m, "mtime(seconds): %ld\n", inode->i_mtime.tv_sec);
+    seq_printf(m, "atime(seconds): %ld\n", inode->i_atime.tv_sec);
+    seq_printf(m, "ctime(seconds): %ld\n", inode->i_ctime.tv_sec);
 }
 
 static int show(struct seq_file *m, void *v)
 {
-    print_fp_state(m);
+    char * path_name = data;
+    pr_info("inode_mod: path name is %s\n", path_name);
+    struct inode* inode = get_inode(path_name);
+    if(!inode){
+        pr_info("inode_mod: inode was not find\n");
+        seq_printf(m, "inode by path %s was not find", path_name);
+        return 0;
+    }
+    pr_info("inode_mod: inode number is %lu\n", inode->i_ino);
+    print_inode(m, inode);
     return 0;
 }
 
@@ -97,32 +63,58 @@ static int open(struct inode *inode, struct  file *file)
 }
 
 static const struct file_operations inode_file_ops = {
-    .llseek = seq_lseek,
-    .open = open,
-    .owner = THIS_MODULE,
-    .read = seq_read,
-    .release = single_release,
+        .llseek = seq_lseek,
+        .open = open,
+        .owner = THIS_MODULE,
+        .read = seq_read,
+        .release = single_release,
 };
 
-static int __init debugfs_init(void)
+static ssize_t data_write(struct file *f, const char *buffer,
+                          size_t len, loff_t *offset)
 {
-	subdir = debugfs_create_dir("inode", NULL);
+    ssize_t ret;
+    ret = simple_write_to_buffer(data, PAGE_SIZE, offset, buffer, len);
+    if (ret < 0){
+        pr_info("inode_mod: path_name writing broke.\n");
+	return ret;
+    }
 
-	path_file = debugfs_create_file("path_here", 0222, subdir, NULL,
-			&path_file_ops);
-			
-	inode_file = debugfs_create_file("inode_output", 0622, subdir, NULL,
-			&inode_file_ops);
-
-	return 0;
+    data_len = ret;
+    return len;
 }
 
-static void __exit debugfs_exit(void)
+static ssize_t data_read(struct file *f, char *buffer,
+                         size_t len, loff_t *offset)
 {
-	debugfs_remove_recursive(subdir);
+    return simple_read_from_buffer(buffer, len, offset, data, data_len);
 }
+
+const struct file_operations path_file_ops = {
+        .owner = THIS_MODULE,
+        .write = data_write,
+        .read = data_read,
+};
+
+static int debugfs_init(void)
+{
+    subdir = debugfs_create_dir("inode", NULL);
+    
+    path_file = debugfs_create_file("path_here", 0222, subdir, NULL,
+                                    &path_file_ops);
+
+    inode_file = debugfs_create_file("inode_output", 0622, subdir, NULL,
+                                     &inode_file_ops);
+
+    return 0;
+}
+
+static void debugfs_exit(void)
+{
+    debugfs_remove_recursive(subdir);
+}
+
 
 module_init(debugfs_init);
 module_exit(debugfs_exit);
-
 
